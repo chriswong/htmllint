e912f860dbf10165f38fbe84ae73ea932fd40990
Roarke Lynch <roarkely@gmail.com>
2013-02-18 00:47:39 -0500

    Cleanup

--- web/web.js
+++ web/web.js
@@ -7,7 +7,6 @@ app.engine('html', engines.hogan);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
-
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
 
@@ -15,14 +14,11 @@ app.use('/', function(req, res) {
 	res.render("home", {
 		partials: {
 			workspace: 'workspace',
-			elements: 'elements',
-			rules: 'rules',
+			options: 'options',
 			tests: 'tests',
 			extending: 'extending'
 		}
 	});
 });
-
-
 
 app.listen(process.env.PORT || 3000);
