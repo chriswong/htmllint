e714aeeaad2932e4af129be766d3433b5dd6fef5
Roarke Lynch <roarkely@gmail.com>
2013-02-17 23:04:47 -0500

    Added Web Interface scafolding

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-#!/usr/bin/env node
-var reporter = require('nodeunit').reporters.default;
-reporter.run(['./tests/_all.js']);
+var _ = require('underscore'),
+	lint = require('./lib/htmllint'),
+	fs = require('fs'),
+	util = require('util');
+
+var src = fs.readFileSync(__dirname + "/test.html", "UTF-8"),
+	res = _.sortBy( lint.lint(src), "offset" );
+
+console.log( util.inspect(res, false, 10) );
---
+++ web/web.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var engines = require('consolidate');
+
+var app = express();
+
+app.engine('html', engines.hogan);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
+
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
+app.use('/', function(req, res) {
+	res.render("home", {
+		partials: {
+			workspace: 'workspace',
+			elements: 'elements',
+			rules: 'rules',
+			tests: 'tests',
+			extending: 'extending'
+		}
+	});
+});
+
+
+
+app.listen(process.env.PORT || 3000);
